97f3c9e5c911e9e8ee60c09a5c358c7faa3e2f4f
anggardhanoano <anggardhanoano@gmail.com>
2020-07-22 19:11:25 +0700

    add support for persistor redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,37 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, compose} from 'redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import {persistReducer} from 'redux-persist';
+import createSensitiveStorage from 'redux-persist-sensitive-storage';
 import counterReducer from './counter';
 
-const rootReducer = combineReducers({
-  counter: counterReducer,
+const sensitiveStorage = createSensitiveStorage({
+  keychainService: 'myKeychain',
+  sharedPreferencesName: 'mySharedPrefs',
 });
 
+const counterPersistConfig = {
+  key: 'counter',
+  storage: AsyncStorage,
+};
+
+// TODO: Uncomment this section to use persist secure storage
+// const tokenPersistConfig = {
+//   key: "token",
+//   storage: sensitiveStorage
+// };
+
+const rootReducer = combineReducers({
+  counter: persistReducer(counterPersistConfig, counterReducer),
+});
+
+let composeEnhancers = compose;
+
+if(__DEV__){
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancers());
 };
 
 export default configStore;
